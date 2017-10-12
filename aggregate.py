0e5faffda623d6fb2e6a44731f4e19bf4ac6eeae
Axel Hecht <axel@pike.org>
2017-10-12 16:27:35 +0200

    fix up totals to be max instead of sum

--- aggregate.py
+++ aggregate.py
@@ -12,8 +12,8 @@ if __name__=='__main__':
 
     yearly_w = defaultdict(lambda: defaultdict(int))
     yearly_s = defaultdict(lambda: defaultdict(int))
-    yearly_wt = defaultdict(lambda: defaultdict(int))
-    yearly_st = defaultdict(lambda: defaultdict(int))
+    monthly_wt = defaultdict(lambda: defaultdict(int))
+    monthly_st = defaultdict(lambda: defaultdict(int))
     
     for item in data:
         d_w = yearly_w[item['year']]
@@ -26,8 +26,8 @@ if __name__=='__main__':
                 cat = 'devtools'
             d_w[cat] += file_data['words']
             d_s[cat] += file_data['strings']
-        d_wt = yearly_wt[item['year']]
-        d_st = yearly_st[item['year']]
+        d_wt = monthly_wt[(item['year'], item['month'])]
+        d_st = monthly_st[(item['year'], item['month'])]
         for f, file_data in item['totals'].items():
             cat = 'browser'
             if 'mobile/' in f:
@@ -36,6 +36,16 @@ if __name__=='__main__':
                 cat = 'devtools'
             d_wt[cat] += file_data['words']
             d_st[cat] += file_data['strings']
+
+    # find max for totals in a year
+    yearly_wt = defaultdict(lambda: defaultdict(int))
+    yearly_st = defaultdict(lambda: defaultdict(int))
+    for (year, month), d in monthly_wt.items():
+        for cat, cnt in d.items():
+            yearly_wt[year][cat] = max(yearly_wt[year][cat], cnt)
+    for (year, month), d in monthly_st.items():
+        for cat, cnt in d.items():
+            yearly_st[year][cat] = max(yearly_st[year][cat], cnt)
 
     print "Year\tbrowser\tmobile\tdevtools"
     for year, d in sorted(yearly_w.items()):
@@ -46,6 +56,8 @@ if __name__=='__main__':
     print "Year\tbrowser\tmobile\tdevtools"
     for year, d in sorted(yearly_s.items()):
         print "\t".join([str(year), str(d.get('browser', '')), str(d.get('mobile', '')), str(d.get('devtools', ''))])
+
+    print
 
     print "Year\tbrowser\tmobile\tdevtools"
     for year, d in sorted(yearly_wt.items()):
